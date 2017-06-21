51be9dae3b3e26eda484b7ffa08387602d06f168
Elan Ruusamäe <glen@delfi.ee>
2017-06-21 20:11:38 +0300

    handle NONE from cvs add

--- fedmsg-cvs-hook.py
+++ fedmsg-cvs-hook.py
@@ -43,8 +43,8 @@ class FedmsgCvsHook():
     def buildFileMessage(self, defopts, filename, oldrev, newrev):
         file = {}
         file['filename'] = filename
-        file['old_rev'] = oldrev
-        file['new_rev'] = newrev
+        file['old_rev'] = oldrev if oldrev != 'NONE' else None
+        file['new_rev'] = newrev if newrev != 'NONE' else None
         file['urls'] = self.buildUrlsMessage(defopts, file)
         return file
 
@@ -53,9 +53,11 @@ class FedmsgCvsHook():
         opts.update(file)
         urls = {}
         urls['log_url'] = self.buildUrl('LOG_URL', opts, opts['new_rev'])
-        urls['old_url'] = self.buildUrl('CO_URL', opts, opts['old_rev'])
-        urls['new_url'] = self.buildUrl('LOG_URL', opts, opts['new_rev'])
-        urls['diff_url'] = self.buildUrl('DIFF_URL', opts)
+        if opts['old_rev']:
+            urls['old_url'] = self.buildUrl('CO_URL', opts, opts['old_rev'])
+        urls['new_url'] = self.buildUrl('CO_URL', opts, opts['new_rev'])
+        if opts['old_rev']:
+            urls['diff_url'] = self.buildUrl('DIFF_URL', opts)
         return urls
 
     def buildUrl(self, url, opts, rev = None):
