3463b3dc1c76be0860c3a5cf6f839b716c7ba399
Elan Ruusamäe <glen@delfi.ee>
2017-06-21 19:55:17 +0300

    refactor code into class
    - refactored logic into class,
    - cleaned up result message to keep only intended attributes
    - fixed log message to contain whole message
    - grouped urls to sub-attribute for readable access

--- fedmsg-cvs-hook.py
+++ fedmsg-cvs-hook.py
@@ -12,96 +12,100 @@ import subprocess
 from itertools import izip
 import fedmsg
 
-def parse_config(filename):
-    """Parse configuration file.
+class FedmsgCvsHook():
+    def __init__(self, argv, stdin):
+        self.user, self.module = argv[2:4]
+        self.files = argv[4:]
+        self.commit_msg = self.get_commit_message(stdin)
+        self.config = self.parse_config(argv[1])
 
-    Args:
-      filename: Path to the configuration file to parse.
-    Returns:
-      Dictionary of values defined in the file.
-    """
-    with open(filename) as f:
-        data = f.read()
-        compiled = compile(data, filename, "exec")
-        result = { 'main': sys.modules[__name__] }
-        eval(compiled, result)
-        return result
+    def buildMessage(self):
+        msg = {
+            'user': self.user,
+            'module': self.module,
+            'message' : self.commit_msg,
+            'files': self.buildFilesMessage(),
+        }
+        return msg
 
-# http://stackoverflow.com/a/5389547
-def grouped(iterable, n):
-    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
-    return izip(*[iter(iterable)]*n)
+    def buildFilesMessage(self):
+        c = self.config
+        defopts = { 'url': c['CVSWEB_URL'], 'module': self.module }
+        files = []
+        for filename, oldrev, newrev in self.grouped(self.files, 3):
+            file = self.buildFileMessage(defopts, filename, oldrev, newrev)
+            files.append(file)
 
-"""
-Commit message is multiline with preformatted by CVS client.
-Read up STDIN up to: 'Log Message:\n',
+        return files
 
-Example:
-['Update of /usr/local/cvs/test/slack\n',
- 'In directory localhost:/home/glen/scm/cvs/test-CVSROOT/testdir/slack\n',
- '\n',
- 'Modified Files:\n',
- '\tslack.txt slack2.txt \n',
- 'Log Message:\n',
- '- slack\n',
- '\n']
-"""
-def get_commit_message():
-    lines = sys.stdin.readlines()
+    def buildFileMessage(self, defopts, filename, oldrev, newrev):
+        file = {}
+        file['filename'] = filename
+        file['old_rev'] = oldrev
+        file['new_rev'] = newrev
+        file['urls'] = self.buildUrlsMessage(defopts, file)
+        return file
 
-    for i in range(len(lines)):
-        if lines[i] == "Log Message:\n":
-            break
+    def buildUrlsMessage(self, defopts, file):
+        opts = defopts.copy()
+        opts.update(file)
+        urls = {}
+        urls['log_url'] = self.buildUrl('LOG_URL', opts, { 'rev' : opts['new_rev'] })
+        urls['old_url'] = self.buildUrl('CO_URL', opts, { 'rev' : opts['old_rev'] })
+        urls['new_url'] = self.buildUrl('LOG_URL', opts, { 'rev' : opts['new_rev'] })
+        urls['diff_url'] = self.buildUrl('DIFF_URL', opts)
+        return urls
 
-    return lines[i + 1:]
+    def buildUrl(self, url, opts, extra = {}):
+        return self.config[url] % dict(list(opts.items()) + list(extra.items()))
 
-def get_commit_summary():
-    lines = get_commit_message()
-    return lines[0].strip()
+    def get_commit_message(self, stdin):
+        """
+        Commit message is multiline with preformatted by CVS client.
+        Read up STDIN up to: 'Log Message:\n',
 
-def cut(s, maxlen = 24):
-    if len(s) > maxlen:
-        return s[0:maxlen] + "..."
-    return s
+        Example:
+        ['Update of /usr/local/cvs/test/slack\n',
+         'In directory localhost:/home/glen/scm/cvs/test-CVSROOT/testdir/slack\n',
+         '\n',
+         'Modified Files:\n',
+         '\tslack.txt slack2.txt \n',
+         'Log Message:\n',
+         '- slack\n',
+         '\n']
+        """
+        lines = stdin.readlines()
 
-user, module = sys.argv[2:4]
-files = sys.argv[4:]
-commit_msg = get_commit_summary()
-c = parse_config(sys.argv[1])
+        for i in range(len(lines)):
+            if lines[i] == "Log Message:\n":
+                break
 
-module_url = "%s/%s/" % (c['CVSWEB_URL'], module)
-summary = "in *<%s|%s>* by *%s*: %s" % (module_url, module, user, commit_msg)
+        return ''.join(lines[i + 1:]).strip()
 
-msg = {
-    'user': user,
-    'module': module,
-    'message' : commit_msg,
-    'files': [],
-}
+    def parse_config(self, filename):
+        """Parse configuration file.
 
-defopts = { 'url': c['CVSWEB_URL'], 'module': module }
-for filename, oldrev, newrev in grouped(files, 3):
-    opts = defopts.copy()
-    opts['file'] = filename
+        Args:
+          filename: Path to the configuration file to parse.
+        Returns:
+          Dictionary of values defined in the file.
+        """
+        with open(filename) as f:
+            data = f.read()
+            compiled = compile(data, filename, "exec")
+            result = { 'main': sys.modules[__name__] }
+            eval(compiled, result)
+            return result
 
-    opts['rev'] = newrev
-    opts['log_url'] = c['LOG_URL'] % opts
+    # http://stackoverflow.com/a/5389547
+    def grouped(self, iterable, n):
+        "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+        return izip(*[iter(iterable)]*n)
 
-    opts['rev'] = oldrev
-    opts['old_url'] = c['CO_URL'] % opts
+if len(sys.argv) > 3:
+    hook = FedmsgCvsHook(sys.argv, sys.stdin)
+    msg = hook.buildMessage()
 
-    opts['rev'] = newrev
-    opts['new_url'] = c['CO_URL'] % opts
-    # temporary for formatting
-    del opts['rev']
-
-    opts['old_rev'] = oldrev
-    opts['new_rev'] = newrev
-    opts['diff_url'] = c['DIFF_URL'] % opts
-
-    msg['files'].append(opts)
-
-# until this is in place, add name="relay_inbound": http://paste.fedoraproject.org/155599/74653371/
-# https://github.com/fedora-infra/fedmsg/issues/426
-
-fedmsg.publish(topic='commit', modname='cvs', active=True, name='relay_inbound', msg=msg)
+    # until this is in place, add name="relay_inbound": http://paste.fedoraproject.org/155599/74653371/
+    # https://github.com/fedora-infra/fedmsg/issues/426
+    fedmsg.publish(topic='commit', modname='cvs', active=True, name='relay_inbound', msg=msg)
